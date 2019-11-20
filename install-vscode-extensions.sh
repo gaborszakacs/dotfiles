9babefceb81faac5fdf18e2b1f1b9a4640501f73
Gabor <gabor.szakacs@bitrise.io>
2019-11-20 11:21:05 +0100

    Add VS Code extensions list

---
+++ install-vscode-extensions.sh
@@ -0,0 +1,21 @@
+# General
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension ionutvmi.spacegray-vscode
+code --install-extension vscodevim.vim
+code --install-extension hoovercj.vscode-settings-cycler
+
+# Ruby/Rails
+code --install-extension rebornix.ruby
+code --install-extension bung87.rails
+code --install-extension misogi.ruby-rubocop
+code --install-extension karunamurti.rspec-snippets
+
+# Go
+code --install-extension ms-vscode.Go
+
+# Misc syntax stuff
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension bungcip.better-toml
+code --install-extension zxh404.vscode-proto3
+code --install-extension mikestead.dotenv
+code --install-extension bierner.markdown-mermaid
