c757dd5009745bec4c7245356680df20bba27c5d
Gabor Szakacs <gabor@simp.hu>
2019-12-30 19:37:37 +0100

    Make Tmux navigatione easier
    add mappings and vim copy mode based on
    https://thoughtbot.com/upcase/videos/tmux-navigation

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -11,3 +11,6 @@ ln -s ~/src/dotfiles/vimrc.bundles ~/.vimrc.bundles
 ln -s ~/src/dotfiles/vim/coc-settings.json ~/.vim/coc-settings.json
 ln -s ~/src/dotfiles/vim/ftplugin/ ~/.vim/ftplugin
 ln -s ~/src/dotfiles/vim/syntax/ ~/.vim/syntax
+
+# BIN
+ln -s ~/src/dotfiles/bin/tat /usr/local/bin/tat
