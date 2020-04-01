9883026749b6b24436a56b5f2a18b2550fa2a5a0
Gabor Szakacs <gabor@simp.hu>
2020-04-01 17:08:46 +0200

    Add TS lang server and tslint support

--- install-vim-coc-extensions.sh
+++ install-vim-coc-extensions.sh
@@ -1,1 +1,1 @@
-vim -c 'CocInstall -sync coc-json coc-pairs coc-snippets coc-solargraph rspec-snippets |q'
+vim -c 'CocInstall -sync coc-json coc-pairs coc-snippets coc-solargraph rspec-snippets coc-tsserver coc-tslint-plugin |q'
