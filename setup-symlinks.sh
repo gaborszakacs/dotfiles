0aa49d878f51177773817edfc5a80c9cf9e8e753
Gabor Szakacs <gabor@simp.hu>
2020-09-27 09:53:09 +0200

    Add .gemrc config

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -4,7 +4,6 @@ ln -s ~/src/dotfiles/vscode/* ~/Library/Application\ Support/Code/User/
 # ZSH TMUX
 ln -s ~/src/dotfiles/zshrc ~/.zshrc
 ln -s ~/src/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/src/dotfiles/alacritty ~/.config/alacritty
 
 # VIM
 ln -s ~/src/dotfiles/vimrc ~/.vimrc
@@ -15,3 +14,7 @@ ln -s ~/src/dotfiles/vim/syntax/ ~/.vim/syntax
 
 # BIN
 ln -s ~/src/dotfiles/bin/tat /usr/local/bin/tat
+
+# MISC
+ln -s ~/src/dotfiles/alacritty ~/.config/alacritty
+ln -s ~/src/dotfiles/gemrc ~/.gemrc
